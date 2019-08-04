065b018a411cb4c4dcce560011a96b850f9a2a22
hajnalandor <hajnal.andor1993@gmail.com>
2019-08-04 19:59:31 +0200

    refactoring fingerprint class and build

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Fingerprint_1 = require("./Fingerprint");
-function getFingerprint() {
-    var fingerprint = new Fingerprint_1.default();
-    return fingerprint.create();
-}
-exports.getFingerprint = getFingerprint;
